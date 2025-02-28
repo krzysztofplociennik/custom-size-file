f294380c68dabe3bda7d4ec3f9f15f3cecd9ed70
kplociennik <krzysztof.plociennik@opi.org.pl>
2025-02-28 12:21:17 +0100

    Added a home component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [HomeComponent]
 })
 export class AppComponent {
   title = 'any-size-pdf';
+
+  constructor(
+    private home: HomeComponent
+  ) {}
+
+  generate() {
+    this.home.generatePdf()
+  }
 }
